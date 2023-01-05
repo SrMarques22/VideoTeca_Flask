fd16a52976877ec23e5244dcd685a27f7a9bb788
SrMarques22 <ti.brunom@gmail.com>
2023-01-05 08:26:17 -0300

    First Commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+#Projeto de estudo Flask, criação de uma Videoteca
---
+++ informacoes.py
@@ -0,0 +1,27 @@
+#Flask é um microframework para criação de aplicações WEB
+'''
+O Flask não é um framework simples, ele é bem completo
+
+nucleo do Flesk são(strutura essencial para web):
+
+Jinja
+Werzug
+'''
+
+
+#Página do Flask:
+'''
+https://flask.palletsprojects.com/en/2.2.x/
+'''
+
+#Site de comparação entre Flask e Django:
+
+'''
+https://www.alura.com.br/artigos/django-ou-flask
+'''
+
+#Nome do projeto: Jogoteca
+'''
+site no qual teremos o nome e dados de varios jogos web
+
+'''
