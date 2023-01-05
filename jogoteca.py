6a59f1b348ed21670728772e9b510f1bd1746140
SrMarques22 <ti.brunom@gmail.com>
2023-01-05 08:57:37 -0300

    Finalizando primeira aula, traçando rotas

--- jogoteca.py
+++ jogoteca.py
@@ -1,1 +1,15 @@
 #Projeto Videoteca Flask
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/inicio')
+def ola():
+    return '<h1>Olá Mundo!</h1>'
+
+app.run()
+
+#O route serve para criarmos a rota após o link de acesso, após a criarmos os passos acima, basta dar um RUN
+# e testar no navegador o link disponibilizado no run do pycharm e colocar a /rota no final. ex: http://127.0.0.1:5000/inicio
+
