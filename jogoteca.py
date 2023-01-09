8c7799f39df1175f97e6f8d2b71ff64b5cc0eb75
SrMarques22 <ti.brunom@gmail.com>
2023-01-09 17:38:53 -0300

    Alterando o metodo de GET para Post

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self,nome, categoria, console):
@@ -6,23 +6,46 @@ class Jogo:
         self.cat=categoria
         self.consol=console
 
+#Colocamos a lista com os games fora do DEF para que as variáveis se tornem GLOBAIS
+jogo1 = Jogo('Donkey Kong','Adventure','Super Nintendo')
+jogo2 = Jogo('Shadow of the Colossus','Puzzle','Play Station')
+jogo3 = Jogo('Wild Rift','MOBA','Smartphone')
+lista = [jogo1,jogo2,jogo3]
+
 # Para inicializar, pela primeira vez, uma aplicação feita em Flask, deve ser utilizado através do código abaixo: app = Flask(__name__)
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
+
+#OBS: O APP.ROUTE deve ficar antes do seu respectivo def!
+@app.route('/novo')
+def novo():
+    return  render_template('novo.html', titulo='Novo Game')
+
+@app.route('/')
+def index():
     '''
     Não foi precio adicionar um caminho para o return abaixo, apenas definir o nome do arquivo .html, devido
     ao Pytho já identificar que a estrutura .html fica em nome_projeto\templates, caso o nome do diretório
     não fosse template, ai então teriamos que definir um caminho!
     :return:
     '''
-    jogo1 = Jogo('Donkey Kong','Adventure','Super Nintendo')
-    jogo2 = Jogo('Shadow of the Colossus','Puzzle','Play Station')
-    jogo3 = Jogo('Wild Rift','MOBA','Smartphone')
-    lista = [jogo1,jogo2,jogo3]
+
     return render_template('lista.html',titulo='GameStation', jogos = lista) #dentro da variável titulo, realizamos a insersão do valor atribuido no arquivo lista.html
 
+
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+#Para executar o debugger e facilitar os ajustes sem ter que dar stop e start na aplicação:
+#app.run(debug=True)
 app.run()
 
 
